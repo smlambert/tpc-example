1992fe8ac0607fe98e927c71556007d667a8b26c
smlambert <slambert@gmail.com>
2018-01-24 12:49:09 -0500

    Add simple java file

---
+++ src/net/adoptopenjdk/example/test/ExampleTest.java
@@ -0,0 +1,10 @@
+package net.adoptopenjdk.example.test;
+
+public class ExampleTest {
+
+	public static void main(String[] args) {
+		System.out.print("Example Test Run, Java version: ");
+		System.out.println(System.getProperty("java.version"));
+	}
+
+}
